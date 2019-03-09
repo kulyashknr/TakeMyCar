b5b6b86bfddb1be1aa3b0cacd6263a38996d511f
aigerimzh <ajgerimzumabaeva10@gmail.com>
2019-03-09 11:25:40 +0600

    User Page1

--- src/components/userpage/UserPage.js
+++ src/components/userpage/UserPage.js
@@ -1,14 +1,77 @@
 import React, { Component } from 'react';
-import Button from 'react-bootstrap/Button';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Button, Container , Row, Col, Image, ListGroup, Nav, Navbar} from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Link , } from "react-router-dom";
+import style from './UserPage.css';
 import './UserPage.css'
+
 
 class UserPage extends Component {
     render(){
         return(
             
             <div className="usermain">
-            <div className="backg"></div>
+                <Container>
+                    <Row>
+                        <Col id="nav">
+                            <ListGroup id="nav_list" variant="flush">
+                                <ListGroup.Item>Здравствуйте, John</ListGroup.Item>
+                                <ListGroup.Item>История</ListGroup.Item>
+                                <ListGroup.Item>Избранное</ListGroup.Item>
+                                <ListGroup.Item>Мои тачки</ListGroup.Item>
+                                <ListGroup.Item>Сообщения</ListGroup.Item>
+                                <ListGroup.Item className="add">Добавить тачку</ListGroup.Item>
+                            </ListGroup>
+                        </Col>
+                        <Col id="info">
+                            <Row>
+                                <Col xs lg="2"><img  src="https://i0.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png?fit=256%2C256&quality=100&ssl=1" className={style.userStyle} alt="user"/></Col>
+                                <Col id="main" >
+                                    <div id="main-content">
+                                        <h1 id="user-name">JOHN DOE</h1>
+                                        <div id="line"></div>
+                                        <Container id="stars">
+                                            <Row>
+                                                
+                                                <div>S</div>
+                                                <div>S</div>
+                                                <div>S</div>
+                                                <div>S</div>
+                                            </Row>
+                                        </Container>
+                                        <h1 id="adress">California, LA</h1>
+                                    </div>
+                                </Col>
+                            </Row>
+                            <Row id="second">
+                                <Container>
+                                    <Row>
+                                        <Nav id="navbar" variant="tabs" defaultActiveKey="/home">
+                                            <Nav.Item>
+                                                <Nav.Link eventKey="link-1">Данные</Nav.Link>
+                                            </Nav.Item>
+                                            <Nav.Item>
+                                                <Nav.Link eventKey="link-2">Контакты</Nav.Link>
+                                            </Nav.Item>
+                                            <Nav.Item>
+                                                <Nav.Link eventKey="link-3">Отзывы</Nav.Link>
+                                            </Nav.Item>
+                                        </Nav>
+                                    </Row>
+                                    <Row>
+                                        <ListGroup id="info-text" variant="flush">
+                                            <ListGroup.Item>Пол
+                                                 <h1 id="info-text2">Мужской</h1>
+                                            </ListGroup.Item>
+                                            <ListGroup.Item>Дата рождения
+                                                <h1 id="info-text2">14/07/1985</h1>
+                                            </ListGroup.Item>
+                                        </ListGroup>
+                                    </Row>
+                                </Container>
+                            </Row>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
 // import * as serviceWorker from './serviceWorker';
 // libraries for react bootstrap 4 ... 
 ReactDOM.render(
